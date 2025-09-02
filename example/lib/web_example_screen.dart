defb69be12fef48c317c509e24e1a8cac9205d50
SwanFlutter1993 <swan.dev1993@gmail.com>
2025-09-01 21:50:25 -0700

    fix yaml

--- example/lib/web_example_screen.dart
+++ example/lib/web_example_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_master/permission_master.dart';
 
-
 /// Web-specific example screen for demonstrating browser permission handling
 class WebExampleScreen extends StatefulWidget {
   const WebExampleScreen({super.key});
